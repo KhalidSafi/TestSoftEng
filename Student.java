52f63a2ec57954e1a5094b8267829042221c7c5d
KhalidSafi <78820210+KhalidSafi@users.noreply.github.com>
2021-03-22 12:36:37 +0200

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,17 @@
+Class Student{
+
+public Student(int id, string name){
+
+this.id = id;
+this.name = name;
+
+}
+
+
+private int id;
+private String name;
+
+
+public void getCourse(Course c){
+System.out.println("Congrats, you registered in this course.");}
+
